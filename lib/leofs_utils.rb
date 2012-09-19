a1e9f5071569a32bebab4402eb322dca950b94d1
Glass_saga <glass.saga@gmail.com>
2012-09-19 13:01:55 +0900

    first commit

--- lib/leofs_utils.rb
+++ lib/leofs_utils.rb
@@ -1,5 +1,28 @@
-require "leofs_utils/version"
-
 module LeofsUtils
-  # Your code goes here...
+  VERSION = "0.0.1"
+
+  class Manager
+    def initialize(host, port, slave_port=nil)
+      @host = host
+      @port = port
+      @slave_port = slave_port
+      @socket = TCPSocket.new(@host, @port)
+    end
+
+    attr_reader :host, :port, :slave_port
+
+    def send_data(data)
+      @socket.write(data) 
+    end
+
+    def read
+      @socket.read
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  m = LeofsUtils::Manager.new("localhost", 10010)
+  m.send_data("status\n")
+  m.read
 end
