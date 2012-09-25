b1271222a9ee94b274d9f39913c59c4626f5e8d5
yosukehara <yousuke.hara@gmail.com>
2012-09-25 09:20:46 +0900

    Change version

--- lib/leofs_manager_client.rb
+++ lib/leofs_manager_client.rb
@@ -26,7 +26,7 @@ require "time"
 require_relative "leofs_manager_client/leofs_manager_models"
 
 module LeoFSManager
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 
   class Remover
     def initialize(data)
