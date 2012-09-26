9d54cdeab480cbeace13613b91b6b75c9c69b003
yosukehara <yousuke.hara@gmail.com>
2012-09-26 21:01:33 +0900

    Change version

--- lib/leofs_manager_client.rb
+++ lib/leofs_manager_client.rb
@@ -26,7 +26,7 @@ require "time"
 require_relative "leofs_manager_client/leofs_manager_models"
 
 module LeoFSManager
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 
   # Class for close TCP socket on GC.
   class Remover
