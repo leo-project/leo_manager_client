88a8a9900c672cbcf22685eda0e9533ce66efef5
Glass_saga <glass.saga@gmail.com>
2013-02-21 18:28:22 +0900

    update version

--- lib/leofs_manager_client.rb
+++ lib/leofs_manager_client.rb
@@ -26,7 +26,7 @@ require "time"
 require_relative "leofs_manager_client/leofs_manager_models"
 
 module LeoFSManager
-  VERSION = "0.2.14"
+  VERSION = "0.2.16"
 
   class Client
     CMD_VERSION          = "version"
